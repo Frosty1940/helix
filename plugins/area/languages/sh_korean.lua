ba4b3ecca1ca16e0d519026800566cac838a443d
Han Myeong-Ho <thks1018@gmail.com>
2020-09-16 14:21:21 +0900

    Add Korean to area plugin

---
+++ plugins/area/languages/sh_korean.lua
@@ -0,0 +1,16 @@
+LANGUAGE = {
+	area = "구역",
+	areas = "구역",
+	areaEditMode = "구역 수정 모드",
+	areaNew = "새 구역",
+	areaAlreadyExists = "그 이름을 가진 구역이 이미 있습니다!",
+	areaDoesntExist = "그 이름을 가진 구역이 없습니다!",
+	areaInvalidType = "올바르지 않은 구역 종류입니다!",
+	areaEditTip = "왼쪽 클릭으로 구역 생성을 시작합니다. 오른쪽 클릭으로 나갑니다.",
+	areaFinishTip = "다시 클릭하면 구역 그리기를 끝냅니다. 오른쪽 클릭으로 뒤로 돌아갑니다.",
+	areaRemoveTip = "지금 들어가 있는 구역을 삭제하려면 장전 버튼을 누르세요.",
+	areaDeleteConfirm = "정말 \"%s\" 구역을 삭제합니까?",
+	areaDelete = "구역 삭제",
+
+	cmdAreaEdit = "구역 수정 모드로 들어갑니다."
+}
